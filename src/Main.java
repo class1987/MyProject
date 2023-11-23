2767c10d174d1fd6fee5570d5dac24a156d4ac55
tester1 <tester1@test.ru>
2023-11-23 16:12:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) throws
+
+        int number = 5;
+        Integer numInteger = 10;
+
+
+        String hello;
+        String world;
+        String c;
+        String d;
+
+        System.out.println
+    }
+
+
+
+    }
+}
